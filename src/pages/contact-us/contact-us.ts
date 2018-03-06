00bd7e776fc4203256388a49c887b4ab5c9e4e8a
developingstar <petr.nocolae812@gmail.com>
2018-03-06 05:57:04 +0100

    Add data to google sheet

--- src/pages/contact-us/contact-us.ts
+++ src/pages/contact-us/contact-us.ts
@@ -1,6 +1,6 @@
 import { Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { NavigationService } from '../../app/app.services.list';
+import { NavigationService, ContactUsService } from '../../app/app.services.list';
 import { MultiSelectUtil } from '../../_utils/multiselect.util';
 @IonicPage()
 @Component({
@@ -22,7 +22,8 @@ export class ContactUsPage {
   ktsSelectSettings: Object = {};
   constructor(public navCtrl: NavController, 
     public navParams: NavParams,
-    public navService: NavigationService
+    public navService: NavigationService,
+    public contactUsService: ContactUsService,
   ) {
   }
 
@@ -35,6 +36,18 @@ export class ContactUsPage {
   }
 
   sendMessage(valid: boolean): void {
+    if (!valid || this.selectedCategory.length === 0) {
+      return;
+    }
+    let data = {
+      name : this.fullName,
+      phone_number: this.phoneNumber,
+      category: this.selectedCategory[0]['itemName'],
+      message: this.message
+    };
+    this.contactUsService.sendData(data).subscribe((res: boolean) => {
+      alert('Send message successfully.');      
+    }, err => console.log('There was an error', err));    
   }
 
   onCategorySelect(item): void {    

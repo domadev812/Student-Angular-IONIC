f8d954f62aa3933b06c1a43b1b94cddbff8102ec
developingstar <petr.nocolae812@gmail.com>
2018-03-08 09:37:13 +0100

    Add address data model

---
+++ src/_models/address.model.ts
@@ -0,0 +1,27 @@
+
+export class Address {
+  id: number;  
+  user_id: number;
+  full_name: string;
+  address_one: string;
+  address_two: string;  
+  city: string;
+  state: string;
+  zip_code: string;
+  phone_number: string;
+  updated_at: Date;      
+  created_at: Date;      
+  constructor(data) {
+    this.id = data.id;
+    this.user_id = data.user_id;
+    this.full_name = data.full_name;
+    this.address_one = data.address_one;
+    this.address_two = data.address_two;
+    this.city = data.city;
+    this.state = data.state;
+    this.zip_code = data.zip_code;
+    this.phone_number = data.phone_number;
+    this.updated_at = data.updated_at;
+    this.created_at = data.created_at;
+  }
+}

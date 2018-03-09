9e97695f7a651ebe2871d3cc81495e3937db87cf
developingstar <petr.nocolae812@gmail.com>
2018-03-09 19:12:28 +0100

    Prototype UI for My Careers

--- src/pages/more-tab-group/my-careers/my-careers.module.ts
+++ src/pages/more-tab-group/my-careers/my-careers.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MyCareersPage } from './my-careers';
+import { MyCareersModule } from '../../../app/app.modules.list';
 
 @NgModule({
   declarations: [
     MyCareersPage,
   ],
   imports: [
+    MyCareersModule,
     IonicPageModule.forChild(MyCareersPage),
   ],
 })

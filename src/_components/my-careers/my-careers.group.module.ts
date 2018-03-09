a2563e1dc9898973e52498a1cba8f3c4b825fe73
developingstar <petr.nocolae812@gmail.com>
2018-03-09 19:13:30 +0100

    Build selected my careers component

---
+++ src/_components/my-careers/my-careers.group.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { SelectedCareersWidgetComponent } from './selected-careers-widget/selected-careers-widget.component';
+
+@NgModule({
+      declarations: [SelectedCareersWidgetComponent],
+  imports: [],
+  exports: [SelectedCareersWidgetComponent]
+})
+export class MyCareersModule {}
---
+++ src/_components/my-careers/selected-careers-widget/selected-careers-widget.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Content } from 'ionic-angular';
+
+@Component({
+  selector: 'selected-careers-widget',
+  templateUrl: 'selected-careers-widget.component.html'
+})
+export class SelectedCareersWidgetComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    
+  }
+}

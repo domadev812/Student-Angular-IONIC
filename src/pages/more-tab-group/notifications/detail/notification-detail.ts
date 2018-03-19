f7b6ead1bd1e71efa25d77b322f363cfe36cbce3
developingstar <petr.nocolae812@gmail.com>
2018-03-19 17:05:43 +0100

    Add mobile header for notification detail

--- src/pages/more-tab-group/notifications/detail/notification-detail.ts
+++ src/pages/more-tab-group/notifications/detail/notification-detail.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController, App } from 'ionic-angular';
+import { Component, ViewChild, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController, App, Content } from 'ionic-angular';
 import { NavigationService, NotificationsService } from '../../../../app/app.services.list';
 import { Model } from '../../../../app/app.models';
 
@@ -9,15 +9,21 @@ import { Model } from '../../../../app/app.models';
   templateUrl: 'notification-detail.html',
 })
 export class NotificationDetailPage {
+  @ViewChild(Content)
+  content: Content;
+
   public notificationId: string;
   public notification: Model.Notification;
+  isScrolled = false;
+  title = 'Notification';
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public navService: NavigationService,
     public notificationService: NotificationsService,
     public viewCtrl: ViewController,
-    private app: App
+    private app: App,
+    public zone: NgZone
   ) {
   }
 
@@ -53,6 +59,24 @@ export class NotificationDetailPage {
       this.navCtrl.pop();
     } else {
       this.goToPage('MyKtsPage', null);
+    }
+  }
+
+  onPageScroll(data) {
+    this.zone.run(() => {
+      if (data.scrollTop > 0) {
+        this.isScrolled = true;
+      } else {
+        this.isScrolled = false;
+      }
+    });
+  }
+  
+  ngAfterViewInit() {
+    if (this.content.ionScroll) {
+      this.content.ionScroll.subscribe((data) => {
+        this.onPageScroll(data);
+      });
     }
   }
 }

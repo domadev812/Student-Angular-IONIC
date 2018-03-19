95d70e0dabf4d514b2c26890eaedd9d42335f30c
developingstar <petr.nocolae812@gmail.com>
2018-03-19 16:46:41 +0100

    Add mobile header to notifications index

--- src/pages/more-tab-group/notifications/notifications.module.ts
+++ src/pages/more-tab-group/notifications/notifications.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NotificationsPage } from './notifications';
-
+import { MobileHeaderModule } from '../../../app/app.modules.list';
 @NgModule({
   declarations: [
     NotificationsPage,
   ],
   imports: [
     IonicPageModule.forChild(NotificationsPage),
+    MobileHeaderModule,
   ],
 })
 export class NotificationsPageModule {}

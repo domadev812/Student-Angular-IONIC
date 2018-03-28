50e119c6fc36f28bc1764db3f7b7aa6a7f138389
developingstar <petr.nocolae812@gmail.com>
2018-03-28 19:28:07 +0200

    Add back arrow to prize show

--- src/pages/prizes/prizeshow/prizeshow.module.ts
+++ src/pages/prizes/prizeshow/prizeshow.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PrizeShowPage } from './prizeshow';
+import { MobileHeaderModule } from '../../../app/app.modules.list';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { PrizeShowPage } from './prizeshow';
   ],
   imports: [
     IonicPageModule.forChild(PrizeShowPage),
+    MobileHeaderModule,
   ],
 })
 export class PrizesPageModule { }
